f7b4da7e7ebc28fa32b0d1406d1583094b56d437
Bulat66 <166387637+Bulat66@users.noreply.github.com>
2024-06-28 16:59:31 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
+// SLIDER
+
 let slider = document.getElementById('slider');
 let slides = document.getElementsByClassName('slide');
-//
-//function sliderMove(e) {
-//	for (let i = 0; i < slideA.length; i++) {
-//			slideA[i].style.width = 0;
-//			i += 1;
-//			slideA[i].style.width = 100 + `px`;
-//			setTimeout(sliderMove(e), 5000);
-//	}
-//}
-//
-//sliderMove(slideA);
-//
 
 let currentSlide = 0;
 
-    function showSlide(n) {
-        slides[currentSlide].classList.remove('active-slide');
-        currentSlide = (n + slides.length) % slides.length;
-        slides[currentSlide].classList.add('active-slide');
-    }
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active-slide');
+}
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+let intervalId = setInterval(nextSlide, 5000);
 
-    function nextSlide() {
-        showSlide(currentSlide + 1);
-    }
 
-    let intervalId = setInterval(nextSlide, 5000);
+// NAVIGATION ADAPTIVE
+
+let menuBtn = document.getElementById('menu-btn');
+let navbar = document.getElementById('nav');
+
+// NAVIGATION BUTTON STYLE
+
+let lines = document.getElementsByClassName('menu-style');
+
+menuBtn.onclick = () => {
+    navbar.classList.toggle('active-nav');
+    navbar.classList.toggle('fa-times');
+
+    lines[1].classList.toggle('cross-1');
+    lines[0].classList.toggle('cross-0');
+    lines[2].classList.toggle('cross-2');
+}
