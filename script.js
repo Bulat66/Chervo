b463337e918cd9644d6fd6791b891f9c02035c10
Bulat66 <166387637+Bulat66@users.noreply.github.com>
2024-07-26 15:43:01 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -30,23 +30,12 @@ menuBtn.onclick = () => {
 }
 
 // DOWNLOAD BTN
-let btnDownloadArray = document.getElementsByClassName('btn-download');
-for(let i = 0; i < btnDownloadArray.length; i++){
-    btnDownloadArray[i].addEventListener('click', () => {
-        var link = document.createElement('a');
+document.getElementById('downloadButton').addEventListener('click', function() {
+    // Создаем ссылку для загрузки файла
+    var link = document.createElement('a');
     link.download = 'карта партнера'; // Установите имя файла для скачивания
     link.href = 'https://disk.yandex.ru/i/lnTV9kZVVmhVug'; // Установите путь к файлу для скачивания
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-    })
-}
-// document.getElementById('downloadButton').addEventListener('click', function() {
-//     // Создаем ссылку для загрузки файла
-//     var link = document.createElement('a');
-//     link.download = 'карта партнера'; // Установите имя файла для скачивания
-//     link.href = 'https://disk.yandex.ru/i/lnTV9kZVVmhVug'; // Установите путь к файлу для скачивания
-//     document.body.appendChild(link);
-//     link.click();
-//     document.body.removeChild(link);
-// });
+});
